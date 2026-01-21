b7e6e3df404187ab2a32b3a932ebb4eea93d1334
Ronny Wouters <ozzy.wouters@telenet.be>
2026-01-21 16:38:22 +0100

    2 pages Home en About met routing

---
+++ src/About.js
@@ -0,0 +1,4 @@
+export default function About() {
+    return <h1>About This Demo</h1>;
+}
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./Home";
+import About from "./About";
+import {HashRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        return (
+            <HashRouter>
+                <nav style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </HashRouter>
+        );
 }
 
 export default App;
